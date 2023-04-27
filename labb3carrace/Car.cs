6623a682982c1b2f2816f4ec970c796312989fb0
Claes Lundin <claeslundin@pm.me>
2023-04-27 15:19:18 +0200

    Add project files.

---
+++ labb3carrace/Car.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb3carrace
+{
+    internal class Car
+    {
+        public string _name { get; set; }
+        public decimal _currentSpeed { get; set; }
+        public decimal _topSpeed { get; set; }
+        public decimal _timeToTopSpeed { get; set; }
+        public decimal _travelledDistance { get; set; }
+        public decimal _totalDistanceTravelled { get; set; }
+        public string  _problem { get; set; }
+        public decimal _time { get; set; }
+        public decimal _timeSpentAccelerating { get; set; }
+
+        public Car(string name, decimal topSpeed, decimal timeToTopSpeed)
+        {
+            _name = name;
+            _topSpeed = topSpeed;
+            _timeToTopSpeed = timeToTopSpeed;
+            _time = 0.05m;
+            _problem = "None";
+        }
+
+        // Start all the calculations of distance, speed and acceleration
+        public void DriveCar()
+        {
+            while (true)
+            {
+                // Every 30 seconds a car have spent driving it could generate an accident 
+                if (_time % 30 == 0)
+                {
+                    GenerateAccident();
+                    _time += 0.05m;
+                }
+                else
+                {
+                    AccelerateCar();
+
+                    // The distance travelled in the last 0,05 second
+                    _travelledDistance = _currentSpeed * 0.05m;
+
+                    Thread.Sleep(50);
+                    _time += 0.05m;
+
+                    // Adding the travelled distance to the total distance
+                    _totalDistanceTravelled += _travelledDistance;
+                }
+            }
+        }
+
+        // Generates a random accident and resets the cars speed and acceleration time to 0
+        public void GenerateAccident()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 51);
+
+            if (num == 5 || num == 10 || num == 15 || num == 20 || num == 25 || num == 30 || num == 35 || num == 40 || num == 45 || num == 50)
+            {
+                _problem = "Engine failure, topspeed permanently lowered by 1";
+                _currentSpeed -= 1;
+                _topSpeed-= 1;
+            }
+
+            if (num == 9 || num == 19 || num == 29 || num == 39 || num == 49)
+            {
+                _currentSpeed = 0;
+                _timeSpentAccelerating = 0;
+                for (int i = 10; i > 0; i--)
+                {
+                    _problem = $"Bird on window, wait {i} sec";
+                    Thread.Sleep(1000);
+                }
+                _problem = "None";
+            }
+
+            if (num == 1 || num == 24)
+            {
+                _currentSpeed = 0;
+                _timeSpentAccelerating = 0;
+                for (int i = 10; i > 0; i--)
+                {
+                    _problem = $"Change tires, wait {i} sec";
+                    Thread.Sleep(1000);
+                }
+                _problem = "None";
+            }
+
+            if (num == 48)
+            {
+                _currentSpeed = 0;
+                _timeSpentAccelerating = 0;
+                for (int i = 30; i > 0; i--)
+                {
+                    _problem = $"Refill fuel, wait {i} sec";
+                    Thread.Sleep(1000);
+                }
+                _problem = "None";
+            }
+        }
+
+        // Calculates the acceleration curve of the car
+        // Is currently done with a cosinus curve
+        public void AccelerateCar()
+        {
+            // Once _currentSpeed reaches _topSpeed number it will no longer accelerate
+            if (_currentSpeed == _topSpeed)
+            {
+                // Do nothing here
+            }
+            else
+            {
+                _timeSpentAccelerating += 0.05m;
+
+                // this calculation is what simulates the acceleration of the car
+                double currentSpeed = Math.Cos(((double)_timeSpentAccelerating / ((double)_timeToTopSpeed / Math.PI)) - Math.PI) * ((double)_topSpeed / 2) + ((double)_topSpeed / 2);
+
+                currentSpeed = Math.Round(currentSpeed);
+                _currentSpeed = (decimal)currentSpeed;
+            }
+        }
+
+        // Prints the "graphical" distance of the car
+        public void PrintDistance()
+        {
+            Console.Write("Start");
+            for (int i = 0; i < 100; i++)
+            {//                                          vvv Change this number to always have 2 less 0s than the distance of the race
+                if (Math.Round(_totalDistanceTravelled / 1000) == i)
+                {
+                    Console.Write("`o##o>");
+                }
+                else
+                {
+                    Console.Write("_");
+                }
+            }
+            Console.WriteLine("Finnish");
+        }
+    }
+}   
---
+++ labb3carrace/Program.cs
@@ -0,0 +1,168 @@
+﻿using System.Runtime.InteropServices;
+using System.Xml.Serialization;
+
+namespace labb3carrace
+{
+    internal class Program
+    {
+        static List<Car> carList = new List<Car>();
+        static bool showStatus = false;
+        
+        // Main method
+        static void Main(string[] args)
+        {
+            AddCar();
+            CarRaceStart();
+            Console.ReadLine();
+        }
+
+        // Start all the threads
+        public static void CarRaceStart()
+        {
+            StartCars();
+            Thread statusSwitch = new Thread(SwitchStatus);
+            statusSwitch.Start();
+            GetCarDistance();
+        }
+
+        // Adds a car with input from user
+        public static void AddCar()
+        {
+            char choice;
+            do
+            {
+                Console.Clear();
+                Console.Write("\n\tCar Name: ");
+                string carName = Console.ReadLine();
+
+                Console.Write("\n\tCar Topspeed: ");
+                decimal.TryParse(Console.ReadLine(), out decimal topSpeed);
+
+                Console.Write("\n\tTime to topspeed: ");
+                decimal.TryParse(Console.ReadLine(), out decimal timeToTopSpeed);
+
+                Car newCar = new Car(carName, topSpeed, timeToTopSpeed);
+                carList.Add(newCar);
+
+                choice = AddAnotherCar();
+                
+            } while (choice != 'N');
+        }
+
+        // Asks the user if they want to add another car
+        public static char AddAnotherCar()
+        {
+            Console.Write("\n\tDo you want to add another car?" +
+                    "\n\t[1]Yes" +
+                    "\n\t[2]No" +
+                    "\n\t:");
+            int.TryParse(Console.ReadLine(), out int choice);
+            switch (choice)
+            {
+                case 1:
+                    return 'Y';
+                case 2:
+                    return 'N';
+                default:
+                    Console.Write("\n\tERROR");
+                    AddAnotherCar();
+                    return 'K';
+            }
+        }
+
+        // Goes through every car in the list and starts a separate thread for it
+        public static void StartCars()
+        {
+            foreach (Car car in carList)
+            {
+                Thread carTread = new Thread(car.DriveCar);
+                carTread.Start();
+            }
+        }
+
+        // Runs in the main thread while any car has not reached the finnish line
+        public static void GetCarDistance() 
+        {
+            do
+            {
+                Thread.Sleep(50);
+                Console.Clear();
+                if (showStatus == true)
+                {
+                    PrintCarsDistances();
+                }
+                if (showStatus == false)
+                {
+                    PrintCarsDistances();
+                    PrintRaceInfo();
+                }   
+                // Will return false as long as no car has reached the finnish line
+            } while (CheckCarsDistances() == false);
+        }
+
+        // Prints out statistics of each car
+        public static void PrintRaceInfo()
+        {
+            foreach (Car car in carList)
+            {
+                Console.Write($"\n\tCar: {car._name}" +
+                    $"\n\tSpeed: {car._currentSpeed}, Distance: {car._totalDistanceTravelled}m, Time spent driving: {car._time}, Problem: {car._problem}");
+                Console.WriteLine();
+            }
+        }
+
+        // Prints out the "graphic" parts `o##o>
+        public static void PrintCarsDistances() 
+        {
+            foreach (Car car in carList)
+            {
+                car.PrintDistance();
+                Console.Write($"{car._name}, Problem: {car._problem}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        // Checks all the cars in the list if they have reach the goal
+        // Will return false as long as no car has reached the finnish line and the true
+        public static bool CheckCarsDistances()
+        {
+            bool hasCarFinnished = false;
+
+            foreach (Car car in carList)
+            {
+                //                                  vvv This number changes the distance of the race
+                if (car._totalDistanceTravelled > 100000)
+                {
+                    PrintWinner(car);
+                    hasCarFinnished = true;
+                    break;
+                }
+            }
+            return hasCarFinnished;
+        }
+
+        // Changes what gets printed to console depending user input
+        public static void SwitchStatus()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "hide")
+                {
+                    showStatus = true;
+                }
+                if (input == "show")
+                {
+                    showStatus = false;
+                }
+            }            
+        }
+
+        // Prints the winner
+        public static void PrintWinner(Car winner)
+        {
+            Console.Write($"\n\t{winner._name} WON!!");
+        }        
+    }
+}
